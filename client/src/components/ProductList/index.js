dfcf3750aaa9399f1224be23a06c9fd52508af48
reetah589 <reetah589@gmail.com>
2021-08-19 12:38:19 -0500

    updated index and home files

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -19,6 +21,29 @@ function ProductList({ currentCategory }) {
       (product) => product.category._id === currentCategory
     );
   }
+
+const [state, dispatch] = useStoreContext();
+
+const { currentCategory } = state;
+
+const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+useEffect(() => {
+  if (data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+  }
+}, [data, dispatch]);
+
+function filterProducts() {
+  if (!currentCategory) {
+    return state.products;
+  }
+
+  return state.products.filter(product => product.category._id === currentCategory);
+}
 
   return (
     <div className="my-2">
