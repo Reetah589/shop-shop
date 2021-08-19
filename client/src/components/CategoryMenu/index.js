e24979d1689106fff6c572dc9edabc60d834792e
reetah589 <reetah589@gmail.com>
2021-08-19 10:58:11 -0500

    global state added

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
 
 function CategoryMenu({ setCategory }) {
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
   const categories = categoryData?.categories || [];
+  const [state, dispatch] = useStoreContext();
+  const { categories } = state;
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   return (
     <div>
