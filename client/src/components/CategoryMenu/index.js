afc26722cf8cac2dd159f03a439e8f1f0751d373
reetah589 <reetah589@gmail.com>
2021-08-21 15:30:30 -0500

    details added

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useStoreContext } from "../../utils/GlobalState";
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu({ setCategory }) {
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
@@ -10,6 +11,8 @@ function CategoryMenu({ setCategory }) {
   const [state, dispatch] = useStoreContext();
   const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     // if categoryData exists or has changed from the response of useQuery, then run dispatch()
@@ -21,6 +24,35 @@ function CategoryMenu({ setCategory }) {
       });
     }
   }, [categoryData, dispatch]);
+
+  useEffect(() => {
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
+
+  if (categoryData) {
+    dispatch({
+      type: UPDATE_CATEGORIES,
+      categories: categoryData.categories
+    });
+    categoryData.categories.forEach(category => {
+      idbPromise('categories', 'put', category);
+    });
+  }
 
   return (
     <div>
